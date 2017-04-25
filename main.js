2f772a7bf3c9b551f0d86cfa7b0c78c0fd5fc324
emstone1 <ethan.marc.stone@gmail.com>
2017-04-24 21:28:21 -0400

    daily

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
 import Expo from 'expo';
-import React from 'react';
+import React, { Component } from 'react';
+import * as firebase from 'firebase'
 import { StyleSheet, Text, View } from 'react-native';
 
-class App extends React.Component {
+// Initialize Firebase
+ firebase.initializeApp({
+       apiKey: "AIzaSyAk4SseEgf0bSmhkZgPje2-EAX-3ay3Xdc",
+       authDomain: "perdiem-a3b62.firebaseapp.com",
+       databaseURL: "https://perdiem-a3b62.firebaseio.com",
+       projectId: "perdiem-a3b62",
+       storageBucket: "perdiem-a3b62.appspot.com",
+       messagingSenderId: "54659155919"
+ });
+
+
+class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up main.js to start working on your app!</Text>
+        <Text>Open up d.js to start working on your app!</Text>
       </View>
     );
   }
